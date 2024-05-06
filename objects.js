c6cd30338466196f6be4950ccc0adf6d0946d473
CALVINCE <calvince.ogwel@gmail.com>
2024-05-06 14:02:12 +0300

    feat: objects.js, iterated through the objects array using the for loop and displayed the contents in the DOM

--- objects.js
+++ objects.js
@@ -8,8 +8,26 @@ function Airplane(manufacturer, model, passengerCapacity, engineType, maximumTak
 
 let airplanes = [
     airplane1 = new Airplane("Boeing", 747 - 400, 524, "Pratt & Whitney PW4056", "875,000 pounds"),
-    airplane2 = new Airplane("Airbus",  "A380", 580, "Rolls Royce Trent 900", "575 tonnes"),
-    airplane3 = new Airplane("Antonov", "AN224 Mriya", "N/A", "Ivchenko Progress Lotarev D-18T", "640 Tonnes"),
+    airplane2 = new Airplane("Airbus", "A380", 580, "Rolls Royce Trent 900", "575 tonnes"),
+    airplane3 = new Airplane("Antonov", "AN224 Mriya", "Cargo Plane", "Ivchenko Progress Lotarev D-18T", "640 Tonnes"),
     airPlane4 = new Airplane("embrarer", "E195", 132, "GE CF34-10E", "61 Tonnes")
-]
-console.log(airplanes)
+];
+
+let currentAirplanes = document.getElementById("airplanes");
+
+let newAirplanes = " ";
+
+for (nA = 0; nA < airplanes.length; nA++) {
+    newAirplanes = newAirplanes + `
+    <tr>
+        <td scope="col">${nA+1}</td>
+        <td scope="col">${airplanes[nA].manufacturer}</td>
+        <td scope="col">${airplanes[nA].model}</td>
+        <td scope="col">${airplanes[nA].passengerCapacity}</td>
+        <td scope="col">${airplanes[nA].engineType}</td>
+        <td scope="col">${airplanes[nA].maximumTakeoffWeight}</td>
+    </tr>
+    `
+};
+
+currentAirplanes.innerHTML = newAirplanes;
