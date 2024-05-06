ea5cc2028c434a3366bf25705220d8b751b24fb5
CALVINCE <calvince.ogwel@gmail.com>
2024-05-06 15:25:51 +0300

    feat objects.js, used localstorage data to persist the new aiplane objects in the airplanes array

--- objects.js
+++ objects.js
@@ -6,58 +6,60 @@ function Airplane(manufacturer, model, passengerCapacity, engineType, maximumTak
         this.maximumTakeoffWeight = maximumTakeoffWeight
 }
 
-let airplanes = [
+let airplanes = JSON.parse(localStorage.getItem('airplanes')) || [
     airplane1 = new Airplane("Boeing", 747 - 400, 524, "Pratt & Whitney PW4056", "875,000 pounds"),
     airplane2 = new Airplane("Airbus", "A380", 580, "Rolls Royce Trent 900", "575 tonnes"),
     airplane3 = new Airplane("Antonov", "AN224 Mriya", "Cargo Plane", "Ivchenko Progress Lotarev D-18T", "640 Tonnes"),
     airPlane4 = new Airplane("embrarer", "E195", 132, "GE CF34-10E", "61 Tonnes")
 ];
 
+function saveAirplanes() {
+    localStorage.setItem('airplanes', JSON.stringify(airplanes));
+}
+
 let currentAirplanes = document.getElementById("airplanes");
 
 let newAirplanes = " ";
 
-for (nA = 0; nA < airplanes.length; nA++) {
-    newAirplanes = newAirplanes + `
-    <tr>
-        <td scope="col">${nA + 1}</td>
-        <td scope="col">${airplanes[nA].manufacturer}</td>
-        <td scope="col">${airplanes[nA].model}</td>
-        <td scope="col">${airplanes[nA].passengerCapacity}</td>
-        <td scope="col">${airplanes[nA].engineType}</td>
-        <td scope="col">${airplanes[nA].maximumTakeoffWeight}</td>
-    </tr>
-    `
-};
-
-currentAirplanes.innerHTML = newAirplanes;
-
-function showNewTable() {
-    for (nA = airplanes.length - 1; nA < airplanes.length; nA++) {
+    for (nA = 0; nA < airplanes.length; nA++) {
         newAirplanes = newAirplanes + `
-    <tr>
-        <td scope="col">${nA + 1}</td>
-        <td scope="col">${airplanes[nA].manufacturer}</td>
-        <td scope="col">${airplanes[nA].model}</td>
-        <td scope="col">${airplanes[nA].passengerCapacity}</td>
-        <td scope="col">${airplanes[nA].engineType}</td>
-        <td scope="col">${airplanes[nA].maximumTakeoffWeight}</td>
-    </tr>
-    `
+        <tr>
+            <td scope="col">${nA + 1}</td>
+            <td scope="col">${airplanes[nA].manufacturer}</td>
+            <td scope="col">${airplanes[nA].model}</td>
+            <td scope="col">${airplanes[nA].passengerCapacity}</td>
+            <td scope="col">${airplanes[nA].engineType}</td>
+            <td scope="col">${airplanes[nA].maximumTakeoffWeight}</td>
+        </tr>
+        `
     };
 
     currentAirplanes.innerHTML = newAirplanes;
-}
 
-function clearForm() {
-    document.getElementById("manufacturer").value = "";
-    document.getElementById("model").value = "";
-    document.getElementById("passengercapacity").value = "";
-    document.getElementById("enginetype").value = "";
-    document.getElementById("maximumtakeoffweight").value = "";
-}
+    function showNewTable(newAirplane) {
+        let newRow = `
+        <tr>
+            <td scope="col">${airplanes.length+1}</td>
+            <td scope="col">${newAirplane.manufacturer}</td>
+            <td scope="col">${newAirplane.model}</td>
+            <td scope="col">${newAirplane.passengerCapacity}</td>
+            <td scope="col">${newAirplane.engineType}</td>
+            <td scope="col">${newAirplane.maximumTakeoffWeight}</td>
+        </tr>
+        `
 
-let submitButton = document.getElementById("submitbutton");
+        currentAirplanes.innerHTML += newRow;
+    }
+
+    function clearForm() {
+        document.getElementById("manufacturer").value = "";
+        document.getElementById("model").value = "";
+        document.getElementById("passengercapacity").value = "";
+        document.getElementById("enginetype").value = "";
+        document.getElementById("maximumtakeoffweight").value = "";
+    }
+
+    let submitButton = document.getElementById("submitbutton");
 
 submitButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -72,9 +74,11 @@ submitButton.addEventListener("click", function (e) {
 
     airplanes.push(newAirplane);
 
+    saveAirplanes();
+
     clearForm();
-    
-    showNewTable();
+
+    showNewTable(newAirplane);
 });
 
 console.log(airplanes);
